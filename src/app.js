c038fdcba0e2cbfdf0205d1e5f0ef2ea6d4fae4f
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-08 20:27:20 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.listen(5000, () => console.log("Listening on port 5000"));
