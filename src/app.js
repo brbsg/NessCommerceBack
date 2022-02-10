06e1d074f2be554196a8c925324ba7becc6834c1
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-09 21:38:39 -0300

    set product routes

--- src/app.js
+++ src/app.js
@@ -8,12 +8,8 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-// const router = Router();
-// router.use(authRouterAdmin);
-
-app.get("/", (req, res) => {
-  res.sendStatus(404);
-});
+const router = Router();
+router.use(authRouterAdmin);
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on port " + process.env.PORT)
