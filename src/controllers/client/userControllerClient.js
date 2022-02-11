9008509fc4781c44c3fa4fc90bc4a5e1ff16e855
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-11 14:24:26 -0300

    fix user controller

--- src/controllers/client/userControllerClient.js
+++ src/controllers/client/userControllerClient.js
@@ -1,35 +1,35 @@
 import db from "../../db.js";
 
 export async function getClient(req, res) {
-    const token = req.authorization;
+  const token = req.headers.authorization;
 
-    if(!token) {
-        return res.sendStatus(401); //Bad Request;
-    };
+  if (!token) {
+    return res.sendStatus(401); //Bad Request;
+  }
 
-    try {
-        const session = await db.collection("client-sessions").findOne({ token });  
+  try {
+    const session = await db.collection("client-sessions").findOne({ token });
     if (!session) {
-        return res.sendStatus(404); //Not Found;
+      return res.sendStatus(404); //Not Found;
     }
 
     const user = await db.collection("client").findOne({ _id: session.userId });
-    if(!user) {
-        res.sendStatus(404); // NotFound;
+    if (!user) {
+      res.sendStatus(404); // NotFound;
     }
-    const ClientCart = await db.collection("carts").findOne({ _id: session.userId });
-    if(!ClientCart) {
-        res.sendStatus(404); // NotFound;
+    const ClientCart = await db
+      .collection("carts")
+      .findOne({ _id: session.userId });
+    if (!ClientCart) {
+      res.sendStatus(404); // NotFound;
     }
     const objectClientData = {
-        name: user.name,
-        cart: ClientCart.products
-    }
+      name: user.name,
+      cart: ClientCart.products,
+    };
     res.send(objectClientData);
-
-    }
-    catch (error){
-        console.log(error);
-        res.sendStatus(500); // Server Error;
-    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500); // Server Error;
+  }
 }
