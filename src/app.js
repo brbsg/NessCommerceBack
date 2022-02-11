7d694b079c2a95d23a9f456f453573690a5592fe
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-11 14:15:15 -0300

    implements admin register products

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-import express, { json, Router } from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 import authRouterAdmin from "./routes/admin/authRouterAdmin.js";
 import productsRouterAdmin from "./routes/admin/productsRouterAdmin.js";
 
 const app = express();
+
 app.use(cors());
 app.use(json());
 
-const router = Router();
-router.use(authRouterAdmin);
-router.use(productsRouterAdmin);
-
-app.use(router);
+app.use(authRouterAdmin);
+app.use(productsRouterAdmin);
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on port " + process.env.PORT)
