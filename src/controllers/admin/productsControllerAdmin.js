3798b927aad2568398cb38dd51232fae41970af3
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-11 18:07:55 -0300

    production

--- src/controllers/admin/productsControllerAdmin.js
+++ src/controllers/admin/productsControllerAdmin.js
@@ -8,7 +8,7 @@ export async function registerProducts(req, res) {
   const token = authorization?.replace("Bearer ", "");
   if (!token) return res.sendStatus(405);
 
-  console.log(token, req.body);
+  console.log("TOKEN:" + token);
   try {
     Jwt.verify(token, process.env.JWT_SECRET);
 
