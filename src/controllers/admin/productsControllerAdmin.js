335ff3791962a207269a7fffea395bd431537965
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-11 17:38:41 -0300

    production

--- src/controllers/admin/productsControllerAdmin.js
+++ src/controllers/admin/productsControllerAdmin.js
@@ -5,24 +5,23 @@ export async function registerProducts(req, res) {
   const token = req.headers.authorization;
   const { name, img, price, description } = req.body;
 
-  try {
-    Jwt.verify(token, process.env.JWT_SECRET);
+  res.send("oi");
+  // try {
+  //   Jwt.verify(token, process.env.JWT_SECRET);
 
-    res.send("oi");
+  //   const dbSession = db.collection("admin-sessions").findOne({ token });
 
-    const dbSession = db.collection("admin-sessions").findOne({ token });
+  //   if (!dbSession) return res.sendStatus(401);
 
-    if (!dbSession) return res.sendStatus(401);
+  //   await db.collection("products").deleteMany({ name });
 
-    await db.collection("products").deleteMany({ name });
+  //   await db
+  //     .collection("products")
+  //     .insertOne({ name, img, price, description });
 
-    await db
-      .collection("products")
-      .insertOne({ name, img, price, description });
-
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(401);
-  }
+  //   res.sendStatus(200);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.sendStatus(401);
+  // }
 }
